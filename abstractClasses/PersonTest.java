e5809f6fd9329431cbcca93b296e670116ce37af
floperry <fengqingsong90@gmail.com>
2016-01-14 21:36:31 +0800

    Add Persontest.java

---
+++ abstractClasses/PersonTest.java
@@ -0,0 +1,23 @@
+package abstractClasses;
+
+/**
+ * This program demonstrates abstract classes.
+ * @author «ÂÀ…
+ * @version 1.10 2016-01-14
+ */
+
+public class PersonTest {
+	
+	public static void main(String[] args) {
+		Person[] people = new Person[2];
+		
+		// fill the people array with Student and Employee objects
+		people[0] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+		people[1] = new Student("Maria Morris", "computer science");
+		
+		// print out names nad descriptions of all Person objects
+		for (Person p : people) {
+			System.out.println(p.getName() + ", " + p.getDescription());
+		}
+	}
+}
